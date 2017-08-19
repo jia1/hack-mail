cde15e548b8b0dbaf7a20751f1a1562c10839516
Jia Yee <jiayeerawr@gmail.com>
2017-08-20 01:47:18 +0800

    Rename and move quickstart.js into js/
    1. quickstart.js is now gmail.js
    2. Update require reference in client.js
    
    Signed-off-by: Jia Yee <jiayeerawr@gmail.com>

--- js/client.js
+++ js/client.js
@@ -1,5 +1,5 @@
 // This file is referenced from the HTML file
 // The working directory is thus where the HTML file is
-var Gmail = require('./quickstart');
+var Gmail = require('./js/gmail.js');
 
 Gmail.executeApi(Gmail.listLabels);
